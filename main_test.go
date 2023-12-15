569eba9766197501fa2caf81f744bc6e1d657bd3
Charliehexx <adi2652002@gmail.com>
2023-12-15 17:09:53 +0530

    added some test cases

--- main_test.go
+++ main_test.go
@@ -2,85 +2,50 @@ package main
 
 import (
 	"bytes"
-	"github.com/stretchr/testify/assert"
-	"gofr.dev/pkg/gofr"
-	"gofr.dev/pkg/gofr/request"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"time"
+
+	"gofr.dev/pkg/gofr"
 )
 
-func TestIntegration(t *testing.T) {
-	go main()
-	time.Sleep(3 * time.Second)
+func TestCreatecar(t *testing.T) {
+	// Create a request body with valid JSON data
+	requestBody := []byte(`{"license_plate": "ABC123", "model": "Sedan", "color": "Blue", "repair_status": "OK"}`)
 
-	tests := []struct {
-		desc       string
-		method     string
-		endpoint   string
-		statusCode int
-		body       []byte
-	}{
-		{"get employees", http.MethodGet, "car", http.StatusOK, nil},
-		{"post employees", http.MethodPost, "car/enter", http.StatusCreated, []byte(`{
-			"id":1,
-			"model":"mahak",
-			"color":"msjce",
-			"repair_status":"928902"
-		}`),
-		}}
-
-	for i, tc := range tests {
-		req, _ := request.NewMock(tc.method, "http://localhost:9000/"+tc.endpoint, bytes.NewBuffer(tc.body))
-
-		c := http.Client{}
-
-		resp, err := c.Do(req)
-		if err != nil {
-			t.Errorf("TEST[%v] Failed.\tHTTP request encountered Err: %v\n%s", i, err, tc.desc)
-			continue
-		}
-
-		if resp.StatusCode != tc.statusCode {
-			t.Errorf("TEST[%v] Failed.\tExpected %v\tGot %v\n%s", i, tc.statusCode, resp.StatusCode, tc.desc)
-		}
-
-		_ = resp.Body.Close()
-	}
-}
-func TestCreateCar(t *testing.T) {
-	// Create a new instance of your application
-	go
-	app := gofr.New()
-
-	// Create a test server
-	server := httptest.NewServer(app.Handler())
-	defer server.Close()
-
-	// Prepare a JSON payload for the POST request
-	payload := `{"license_plate": "ABC123", "model": "Sedan", "color": "Blue", "repair_status": "Pending"}`
-
-	// Make a POST request to the /car/enter endpoint
-	resp, err := http.Post(server.URL+"/car/enter", "application/json", bytes.NewBufferString(payload))
+	// Create a request with the given request body
+	request, err := http.NewRequest("POST", "/car/enter", bytes.NewBuffer(requestBody))
 	if err != nil {
-		t.Fatalf("Error making POST request: %v", err)
+		t.Fatal(err)
 	}
-	defer resp.Body.Close()
 
-	// Verify the response status code is as expected
-	assert.Equal(t, http.StatusOK, resp.StatusCode, "Expected status code %d, got %d", http.StatusOK, resp.StatusCode)
+	// Create a response recorder to capture the response
+	responseRecorder := httptest.NewRecorder()
 
-	// Decode the response body
-	var response interface{}
-	err = json.NewDecoder(resp.Body).Decode(&response)
+	// Create a gofr Context for testing
+	ctx := gofr.NewTestContext(request)
+
+	// Call the Createcar function, passing the response recorder and context
+	Createcar(ctx)
+
+	// Check the HTTP status code
+	if responseRecorder.Code != http.StatusOK {
+		t.Fatalf("Unexpected status code. Expected: %d, Got: %d", http.StatusOK, responseRecorder.Code)
+	}
+
+	// Parse the response body
+	var responseBody map[string]interface{}
+	err = json.NewDecoder(responseRecorder.Body).Decode(&responseBody)
 	if err != nil {
-		t.Fatalf("Error decoding response body: %v", err)
+		t.Fatal(err)
 	}
 
-	// Verify the response content, adjust based on your application logic
-	expectedResponse := "Car entered the garage successfully"
-	assert.Equal(t, expectedResponse, response, "Unexpected response content")
+	// Add your assertions based on the expected response
+	// For example, you might check the content of the response JSON
+	expectedMessage := "Car entered the garage successfully"
+	if responseMessage, ok := responseBody["message"].(string); !ok || responseMessage != expectedMessage {
+		t.Fatalf("Unexpected response. Expected: %s, Got: %s", expectedMessage, responseMessage)
+	}
 
-	// You can add more assertions based on your application logic
 }
